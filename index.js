ea09dacb46f11e51145050c01b800f12a0e73a74
Amritanshu  Verma(The Alpha King) <amritanshu26dec@gmail.com>
2021-04-01 10:38:16 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.static(path.join(__dirname,'public')));
+
+app.listen(port,()=>console.log(`server is start on ${port}`));
---
+++ public/assets/js/experience.js
@@ -0,0 +1,121 @@
+//  Work experience cards
+
+
+const experiencecards = document.querySelector(".experience-cards");
+const exp = [
+  {
+    title: "Developer Student Club Lead",
+    cardImage: "assets/images/experience-page/bhg.png",
+    place: "Google Developers",
+    time: "(July 2019 - Aug 2020 )",
+    desp: "<li>To build & grow student developer community</li><li>As being Developer Student Club Lead of my university,I have led & mentored 300+ student developers through events supported by google developers for influencing new bright minds to learn & build products where I’ve used figma,sketch,Indesign for poster design and other stuffs.</li>",
+  },
+];
+
+const showCards2 = () => {
+  let output = "";
+  exp.forEach(
+    ({ title, cardImage, place, time, desp }) =>
+      (output += `        
+      <ul>
+      <li class="card card1">
+        <img src="${cardImage}" class="featured-image"/>
+        <article class="card-body">
+          <header>
+            <div class="title">
+              <h3>${title}</h3>
+            </div>
+            <p class="meta">
+              <span class="pre-heading">${place}</span><br>
+              <span class="author">${time}</span>
+            </p>
+            <ol>
+              ${desp}
+            </ol>
+          </header>
+        </article>
+      </li>
+    </ul>
+      `)
+  );
+  experiencecards.innerHTML = output;
+};
+document.addEventListener("DOMContentLoaded", showCards2);
+
+
+// Volunteership Cards
+
+const volunteership = document.querySelector(".volunteership");
+const volunteershipcards = [
+  {
+    title: "Google HashCode",
+    cardImage: "assets/images/experience-page/ghashcode2019.png",
+    description:
+      "Responsible for handling and managing Gurukula Kangri University Hub from where 15 Teams have participated in this coding competition",
+  },
+  
+  {
+    title: "30 Days of Flutter",
+    cardImage: "assets/images/experience-page/flutter.png",
+    description:
+      "Helping beginners from their first steps in Flutter to building fully functional cross-platform applications.",
+  },
+  
+];
+
+const showCards = () => {
+  let output = "";
+  volunteershipcards.forEach(
+    ({ title, cardImage, description }) =>
+      (output += `        
+      <div class="card volunteerCard" style="background-image: url(${cardImage});background-repeat: no-repeat, repeat; background-size: cover; background-position: center;">
+      <div class="content">
+          <h2 class="volunteerTitle">${title}</h2>
+          <p class="copy">${description}</p></div>
+      </div>
+      `)
+  );
+  volunteership.innerHTML = output;
+};
+document.addEventListener("DOMContentLoaded", showCards);
+
+
+// Mentorship Card
+
+
+const mentorshipcards = document.querySelector(".mentorship-cards");
+const mentor = [
+  {
+    title: "Google Code-IN",
+    image: "assets/images/experience-page/gcodein.png",
+    time: "12/2019 - 02/2020",
+    desp: "<li>Google Code-IN is a global program focused on introducing students to open source software development.</li><hr /><li>It is a great platform to explore new areas, maybe discover a new career path!</li>",
+  },
+];
+
+const showCards3 = () => {
+  let output = "";
+  mentor.forEach(
+    ({ title, image, time, desp}) =>
+      (output += `        
+      <div class="column mentorshipCard"> 
+      <div class="card card2 mentorshipCardCover">
+        <img src="${image}" class="card-img-top" alt="..."  width="64" height="300">
+        <div class="information">
+        <div class="card-body">
+          <h5 class="card-title">${title}</h5>
+          <p class=""sub-title">${time}</p>
+        </div>
+        <div class="more-information">
+        <ul class="list-group list-group-flush p-0 right-aligned">
+          <div class="list-group-item card2 disclaimer">${desp}</div>
+        </ul>
+        </div>
+        </div>
+      </div>
+      </div>
+      `)
+  );
+  mentorshipcards.innerHTML = output;
+};
+document.addEventListener("DOMContentLoaded", showCards3);
